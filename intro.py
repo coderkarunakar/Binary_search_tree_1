1be85b33f0d465b7975b47237d3feaef4449c4e3
Karunakar chembeti <114292518+coderkarunakar@users.noreply.github.com>
2024-01-09 08:13:28 +0530

    intro_bst

---
+++ intro.py
@@ -0,0 +1,11 @@
+#Binary_search_Tree introduction 
+#in order to find a node ex:5 we will traverse all the elements in the tree step by step which is linear search 
+#in linear search in order to find some element we will be traversing all the elements which makes 
+# O(n) work
+#in binary search first we will sort it then we will find its mid point and then we will decide to travel left or right based on search value
+#similarly we will sort the tree in this binary search tree as well
+#i.e every thing smaller is on left and every thing larger is on right
+#and root is  in the middle
+#and this condition is not only for the root it also for the each and every node as well 
+
+#please take a look in class notes as well
